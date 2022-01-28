24fd7b7b4d809f9c010485da053ba940b0607bea
AntonDidrikhs <didrikhsant@gmail.com>
2022-01-28 17:35:44 +0100

    New SeedData + Index change

--- GameStore/Models/SeedData.cs
+++ GameStore/Models/SeedData.cs
@@ -27,11 +27,11 @@ namespace GameStore.Models
                     new Game
                     {
                         Title = "Among Us",
-                        Descriprion = "Sus, soosy baka, amogus... SUS",
+                        Descriprion = "A Mafia-like social game based on deseption and deduction",
                         ReleaseDate = DateTime.Parse("2018-11-13"),
-                        Genre = "Psychological Horror",
+                        Genre = "Social Deduction",
                         Price = 19.99M,
-                        Studio = "Antichrist Inc."
+                        Studio = "Innersloth"
                     },
                     new Game
                     {
@@ -41,6 +41,42 @@ namespace GameStore.Models
                         Genre = "FPS",
                         Price = 39.99M,
                         Studio = "Valve"
+                    },
+                    new Game
+                    {
+                        Title = "Rust",
+                        Descriprion = "The only aim in Rust is to survive. Everything wants you to die - the island’s wildlife and other inhabitants, the environment, other survivors. Do whatever it takes to last another night.",
+                        ReleaseDate = DateTime.Parse("2018-2-8"),
+                        Genre = "Survival",
+                        Price = 39.99M,
+                        Studio = "Facepunch Studios"
+                    },
+                    new Game
+                    {
+                        Title = "The Forest",
+                        Descriprion = "As the lone survivor of a passenger jet crash, you find yourself in a mysterious forest battling to stay alive against a society of cannibalistic mutants.",
+                        ReleaseDate = DateTime.Parse("2018-4-30"),
+                        Genre = "Survival",
+                        Price = 29.99M,
+                        Studio = "Endnight Games Ltd"
+                    },
+                    new Game
+                    {
+                        Title = "Dragon Age Inqusition",
+                        Descriprion = "When the sky opens up and rains down chaos, the world needs heroes. Become the savior of Thedas in Dragon Age: Inquisition. You are the Inquisitor, tasked with saving the world from itself. But the road ahead is paved with difficult decisions. Thedas is a land of strife.",
+                        ReleaseDate = DateTime.Parse("2014-11-14"),
+                        Genre = "RPG",
+                        Price = 49.99M,
+                        Studio = "BioWare"
+                    },
+                    new Game
+                    {
+                        Title = "The Witcher 3",
+                        Descriprion = "As war rages on throughout the Northern Realms, you take on the greatest contract of your life — tracking down the Child of Prophecy, a living weapon that can alter the shape of the world.",
+                        ReleaseDate = DateTime.Parse("2015-5-18"),
+                        Genre = "RPG",
+                        Price = 29.99M,
+                        Studio = "CD PROJEKT RED"
                     }
                     );
                 context.SaveChanges();
